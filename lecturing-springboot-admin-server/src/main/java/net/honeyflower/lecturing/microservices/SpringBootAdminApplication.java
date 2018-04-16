8c5cfead4dc1a27afd88cf4101377a9a52cb93c5
Eugene <Eugene@___>
2018-04-16 12:55:40 +0300

    spring-boot-admin-server

---
+++ lecturing-springboot-admin-server/src/main/java/net/honeyflower/lecturing/microservices/SpringBootAdminApplication.java
@@ -0,0 +1,16 @@
+package net.honeyflower.lecturing.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+
+	}
+
+}
